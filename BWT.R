a13fa80f9a1b016a12f74d2a1e8021fb0e513c47
unknown <tthloc.stu13@medvnu.edu.vn>
2020-12-15 10:16:12 +0700

    bwt

---
+++ BWT.R
@@ -0,0 +1,10 @@
+a <- "banana"
+words <- strsplit(a, split = "")[[1]]
+for (i in 1:length(words)) {
+  suff <- strsplit(a, split = "")[[1]][length(words)]
+  pre <- strsplit(a, split = "")[[1]][1:(length(words)) - 1]
+  pre <- paste(pre, collapse = '')
+  word <- paste0(suff, pre)
+  a <- word
+  print(word)
+}

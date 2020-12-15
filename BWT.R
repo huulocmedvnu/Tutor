44d073c7ca69baeac3144ba1cf30a9206f6c2cfa
unknown <tthloc.stu13@medvnu.edu.vn>
2020-12-15 10:29:23 +0700

    bwt 2

--- BWT.R
+++ BWT.R
@@ -1,10 +1,15 @@
 a <- "banana"
 words <- strsplit(a, split = "")[[1]]
+lst <- c()
 for (i in 1:length(words)) {
   suff <- strsplit(a, split = "")[[1]][length(words)]
   pre <- strsplit(a, split = "")[[1]][1:(length(words)) - 1]
   pre <- paste(pre, collapse = '')
   word <- paste0(suff, pre)
   a <- word
-  print(word)
+  lst <- c(lst, word)
 }
+
+sorted_lst <- sort(lst)
+
+
